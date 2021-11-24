be9189547fc301edf027dde137f404496ddeccff
Presy Lord Kidlat <kidlat123@gmail.com>
2021-11-25 01:17:31 +0800

    added modal and date

--- app.js
+++ app.js
@@ -17,8 +17,27 @@ const recordSchema = new mongoose.Schema({
 
 const Record = mongoose.model("Record", recordSchema);
 
+const today = new Date();
+const time = {
+  hour: "2-digit",
+  minute: "2-digit",
+};
+const day = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+};
+
+const currentDay = today.toLocaleString("en-US", day);
+const currentTime = today.toLocaleString("en-US", time);
+console.log(currentTime, currentDay);
+
 app.get("/", function (req, res) {
   res.render("home");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.employeeID);
 });
 
 app.listen(3000, function () {
