0e228e58978710ffd475869cd53000ecc24389a1
Presy Lord Kidlat <kidlat123@gmail.com>
2021-11-28 23:21:13 +0800

    time in and out

--- app.js
+++ app.js
@@ -21,19 +21,14 @@ app.set("view engine", "ejs");
 
 //////////////////////////// Current Date ///////////////////////
 let current_datetime = new Date();
-let date =
+let dateToday =
   current_datetime.getFullYear() +
   "-" +
   (current_datetime.getMonth() + 1) +
   "-" +
-  current_datetime.getDate() +
-  " " +
-  current_datetime.getHours() +
-  ":" +
-  current_datetime.getMinutes() +
-  ":" +
-  current_datetime.getSeconds();
-let time =
+  current_datetime.getDate();
+
+let timeToday =
   current_datetime.getHours() +
   ":" +
   current_datetime.getMinutes() +
@@ -79,48 +74,44 @@ app.post("/", function (req, res) {
 
 app.get("/:user", function (req, res) {
   const user = req.params.user;
-  db.select("*")
-    .from("user_entry")
-    .where("username", username)
-    .then((user) => {
-      res.render("dtr", { user: user });
+  db("user_entry")
+    .insert({
+      username: user,
+      date: dateToday,
+    })
+    .then((response) => {
+      console.log("logged successfully");
     });
+  res.render("dtr", { user: user });
 });
-
 app.post("/:user", function (req, res) {
   const user = req.params.user;
   const time = req.body.time;
   if (time == "in") {
     db("user_entry")
-      .insert({
-        username: user,
-        date: date,
-        // time_in: time,
+      .where("username", "=", user)
+      .whereNotNull("date")
+      .whereNull("time_in")
+      .update({
+        time_in: timeToday,
       })
-      .then((response) => {
-        res.redirect("/" + user);
-      })
-      .catch((err) => console.log("error"));
+      .catch((err) => console.log(err));
+    res.redirect("/" + user);
   } else {
-    console.log(err);
+    db("user_entry")
+      .where("username", "=", user)
+      .whereNotNull("date")
+      .whereNull("time_out")
+
+      .update({
+        time_out: timeToday,
+      })
+
+      .catch((err) => console.log(err));
+    res.redirect("/" + user);
   }
 });
 
 app.listen(3000, function () {
   console.log("Server started at port 3000.");
 });
-
-// CREATE TABLE users (
-//   user_id SERIAL NOT NULL PRIMARY KEY,
-//   first_name VARCHAR(50) NOT NULL,
-//   last_name VARCHAR(50) NOT NULL,
-//   email VARCHAR(50) NOT NULL
-// );
-
-// CREATE TABLE login (
-//   id SERIAL NOT NULL PRIMARY KEY,
-//   user_id INT NOT NULL,
-//   pin INT NOT NULL,
-//   FOREIGN KEY (user_id) REFERENCES users(user_id)
-
-// );
