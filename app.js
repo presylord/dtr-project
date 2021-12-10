99e0a83e90807f87e4712b2058c7e8a884384314
presylord <kidlat123@gmail.com>
2021-12-10 10:22:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const _ = require("lodash");
 const ejs = require("ejs");
 
@@ -7,22 +6,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-mongoose.connect("mongodb://localhost:27017/employeesDB");
-
-const recordSchema = new mongoose.Schema({
-  user: String,
-  pin: Number,
-  entry: [
-    {
-      date: String,
-      timeIn: String,
-      timeOut: String,
-    },
-  ],
-});
-
-const Record = mongoose.model("Record", recordSchema);
 
 //////////////////////////// Current Date ///////////////////////
 const today = new Date();
@@ -39,6 +22,13 @@ const day = {
 const currentDay = today.toLocaleString("en-US", day);
 const currentTime = today.toLocaleString("en-US", time);
 
+///////////////////////////// Database ///////////////////////////
+
+const users = [];
+const entries = [];
+
+console.log(users);
+
 ///////////////////////////////////// REGISTRATION ///////////////////////////
 app.get("/register", function (req, res) {
   res.render("register");
@@ -47,20 +37,14 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const user = req.body.employeeName;
   const pin = req.body.pin;
-  const newUser = new Record({
+
+  users.push({
     user: user,
     pin: pin,
-    entry: [],
+    entries: [],
   });
-
-  newUser.save(function (err) {
-    if (!err) {
-      console.log("Employee added succesfully.");
-      res.redirect("/");
-    } else {
-      console.log(err);
-    }
-  });
+  console.log(users);
+  res.redirect("/");
 });
 ///////////////////////////////////// Functions ////////////////////////////////////
 app.get("/", function (req, res) {
@@ -70,64 +54,70 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   const name = req.body.employeeName;
   const pin = req.body.pin;
-  Record.findOne({ user: name, pin: pin }, function (err, found) {
-    if (found) {
-      const user = _.lowerCase(found.user);
-      res.redirect("/" + user);
-    } else {
-      console.log(err);
+  console.log(users[0].user);
+  for (var x = 0; x == users.length; x++) {
+    if (users[x].user == name && users[x] == pin) {
+      var login = true;
     }
-  });
+  }
+  if (login == true) {
+    res.redirect("/" + name);
+  } else {
+    res.redirect("/");
+    console.log("Wrong User or Password!");
+  }
 });
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 app.get("/:user", function (req, res) {
   const user = req.params.user;
-  Record.findOne({ user: user }, function (err, record) {
-    res.render("dtr", {
-      user: user,
-      // date: record.entry.date,
-      // timeIn: record.entry.timeIn,
-      // timeOut: record.entry.timeOut,
-    });
-  });
+  res.render("dtr", { user: user });
+  // Record.findOne({ user: user }, function (err, record) {
+  //   res.render("dtr", {
+  //     user: user,
+  //     // date: record.entry.date,
+  //     // timeIn: record.entry.timeIn,
+  //     // timeOut: record.entry.timeOut,
+  //   });
+  // });
 });
 
 app.post("/:user", function (req, res) {
   const user = req.params.user;
   const time = req.body.time;
+
   if (time == "in") {
-    Record.findOneAndUpdate(
-      { user: user },
-      {
-        entry: [
-          {
-            date: currentDay,
-          },
-        ],
-      },
-      function (err, found) {
-        res.redirect("/" + user);
-      }
-    );
+    // Record.findOneAndUpdate(
+    //   { user: user },
+    //   {
+    //     entry: [
+    //       {
+    //         date: currentDay,
+    //       },
+    //     ],
+    //   },
+    //   function (err, found) {
+    //     res.redirect("/" + user);
+    //   }
+    // );
   }
   // try making the entries into a different array
   else if (time == "out") {
-    Record.findOneAndUpdate(
-      { user: user, timeOut: "" },
-      {
-        entry: {
-          date: date,
-          timeIn: timeIn,
-          timeOut: currentTime,
-        },
-      },
-      { new: true },
-      function (err, found) {
-        res.redirect("/" + user);
-      }
-    );
+    // Record.findOneAndUpdate(
+    //   { user: user, timeOut: "" },
+    //   {
+    //     entry: {
+    //       date: date,
+    //       timeIn: timeIn,
+    //       timeOut: currentTime,
+    //     },
+    //   },
+    //   { new: true },
+    //   function (err, found) {
+    //     res.redirect("/" + user);
+    //   }
+    // );
   }
 });
 
